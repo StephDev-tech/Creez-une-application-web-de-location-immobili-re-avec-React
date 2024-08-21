7985d8226845f377b5b38b001a547d7fa6fbd4ea
ShanksLeBlack <149971596+StephDev-tech@users.noreply.github.com>
2024-08-21 19:01:22 -0400

    développement de la parti info logement et notation et tag

--- src/pages/Housesheet.jsx
+++ src/pages/Housesheet.jsx
@@ -1,23 +1,39 @@
-import image from "../assets/logo_kasa.svg";
 import { useParams } from "react-router-dom";
 import Carousel from "../components/Carousel";
+import Tags from "../components/Tags.jsx";
+import Ratings from '../components/Ratings.jsx'
 import data from "../assets/data.json";
 
 const Housesheet = () => {
 	const { houseId } = useParams();
 	const result = data.find((element) => element.id === houseId);
+	const tags = result.tags;
 
 	return (
 		<div className="housesheet">
-            <Carousel />
+			<Carousel />
 			<div className="housesheet-info">
 				<div className="housesheet-title">
 					<h2 className="title">{result.title}</h2>
 					<h3 className="subtitle">{result.location}</h3>
 				</div>
-				<div>
-					<p className="profil-name">Nom du profil</p>
-					<img src={image} alt="logo Kasa" className="profil-picture" />
+				<div className="profil">
+					<p className="profil-name">{result.host.name}</p>
+					<img
+						src={result.host.picture}
+						alt="logo Kasa"
+						className="profil-picture"
+					/>
+				</div>
+			</div>
+			<div className="tagsRatings">
+				<div className="div-tags">
+					{tags.map((tag) => (
+						<Tags tagName={tag} />
+					))}
+				</div>
+				<div className="div-ratings">
+					<Ratings rate={result.rating}/>
 				</div>
 			</div>
 		</div>
